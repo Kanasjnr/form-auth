a8cf1f7cc9bef788c631b83bf1bb1e14b9a9e30f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-20 09:29:17 +0100

    d

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,4 +4,12 @@ const useLocalStorage = (key, initValue) => {
   const [value, setValue] = useState(
     JSON.parse(localStorage.getItem(key)) || initValue
   );
+
+    useEffect(() => {
+        localStorage.setItem(key,JSON.stringify(value))
+    },[key,value])
+
 };
+
+
+export default useLocalStorage
