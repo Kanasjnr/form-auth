2fd450d96f88357a9430831df1ee9fbe0255d21f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-20 11:35:51 +0100

    d

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -5,35 +5,34 @@ import useAuth from "../hooks/useAuth";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 const PersistLogin = () => {
-   const [isLoading, setIsLoading] = useState(true);
-   const refresh = useRefreshToken();
-   const { auth } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+  const [persist] = useLocalStorage("persist", false);
 
-   useEffect(() => { 
-      let isMounted = true
-      const verifyRefreshToken = async () => {
-         try {
-            await refresh();
-         } catch (error) {
-            console.log(error);
-         } finally {
-            setIsLoading(false);
-         }
-      };
-      !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
-      return () =>   isMounted = false
-   }, []);
+  useEffect(() => {
+    let isMounted = true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    return () => (isMounted = false);
+  }, []);
 
-   useEffect(() => {
-      console.log(`isLoading: ${isLoading}`);
-      console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
-   }, [isLoading]);
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+  }, [isLoading]);
 
-   return (
-        <>
-            {!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}
-        </>
-   );
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
 };
 
 export default PersistLogin;
