3acfc3db9673b194d6f388226d77e99e7d9b25d2
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-20 10:41:07 +0100

    e

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
    const userRef = useRef();
    const errRef = useRef();
 
-   const [user, setUser] =useInput('')//useState(""); 
+   const [user, resetUser, setAttribs] =useInput('')//useState(""); 
    const [pwd, setPwd] = useState("");
    const [errMsg, setErrMsg] = useState("");
 
