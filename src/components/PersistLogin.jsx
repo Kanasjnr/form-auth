585d1b823a35b7df50820021b28408f78e781b9f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-20 11:33:41 +0100

    z

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const PersistLogin = () => {
    const [isLoading, setIsLoading] = useState(true);
