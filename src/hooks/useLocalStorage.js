582b68c2b44739c38ae338b3f122c7089f7a7c2d
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-20 10:06:05 +0100

    d

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,28 +1,29 @@
 import { useState, useEffect } from "react";
 
-
 const getLocalValue = (key, initValue) => {
-        //SSR Next Js
-    if (typeof window === 'undefined') return initValue
+  //SSR Next Js
+  if (typeof window === "undefined") return initValue;
 
-    //If A value is already stored
+  //If A value is already stored
 
-    const localValue = JSON.parse(localStorage.getItem(key))
+  const localValue = JSON.parse(localStorage.getItem(key));
 
-    if (localValue) return localValue
+  if (localValue) return localValue;
 
-    // return result of a function
-    if(initValue instanceof Function) return initValue
-}
-
+  // return result of a function
+  if (initValue instanceof Function) return initValue;
+  return initValue;
+};
 
 const useLocalStorage = (key, initValue) => {
-  const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || initValue);
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(key)) || initValue
+  );
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
-  return [value, setValue]
+  return [value, setValue];
 };
 
 export default useLocalStorage;
